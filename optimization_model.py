79cf28a930b7024741cf630bac9b46ab7caa8add
andres lemus <leandres1985@gmail.com>
2025-03-11 19:13:04 +0100

    updated optimisation_model

--- optimization_model.py
+++ optimization_model.py
@@ -3,38 +3,39 @@ from data_loader import DataLoader
 
 
 class OptimizationModel:
-    """Clase que representa el modelo de optimización."""
+    """Class representing the optimisation model"""
 
     def __init__(self, q_gl, q_fluid_wells):
         self.q_gl = q_gl
         self.q_fluid_wells = q_fluid_wells
         #self.prob = pulp.LpProblem("Maximizar_Suma_Wells", pulp.LpMaximize)
-        #self.y_wells = self.definir_variables()
-        #self.construir_modelo()
+        #self.y_wells = self.define_variables()
+        #self.build_model()
         #self.agregar_restricciones()
 
     def define_optimisation_problem(self):
-        self.prob = pulp.LpProblem("Maximizar_Suma_Wells", pulp.LpMaximize)
+        self.prob = pulp.LpProblem("Maximise the sum of wells' production", pulp.LpMaximize)
 
 
-    def definir_variables(self):
-        """Define las variables binarias para cada well y qgl."""
-        return [
+    def define_variables(self):
+        """Define the binary variables for each q_gl and q_fluid"""
+        binary_variables = [
             [pulp.LpVariable(f'y{well_index}_{i}', cat='Binary') for i in range(len(self.q_gl))]
             for well_index in range(len(self.q_fluid_wells))
         ]
+        return binary_variables
 
-    def construir_modelo(self):
-        """Define la función objetivo a maximizar."""
-        variables = self.definir_variables()
+    def build_model(self):
+        """Defines the objective function to maximise"""
+        variables = self.define_variables()
         self.prob += pulp.lpSum(
             variables[i][j] * self.q_fluid_wells[i][j]
             for i in range(len(self.q_fluid_wells))
             for j in range(len(self.q_gl))
-        ), "Funcion_Objetivo"
+        ), "Objective function"
         return self.prob
 
-    def agregar_restricciones(self):
+    def add_constraints(self):
         """Asegura que cada well seleccione exactamente un valor de qgl."""
         for index, col in enumerate(self.q_fluid_wells):
             self.prob += pulp.lpSum(col) == 1, f"Restriccion_Seleccion_Unica_{index}"
@@ -64,11 +65,12 @@ if __name__ == "__main__":
     q_gl,q_fluid_wells = data.load_data()
     model = OptimizationModel(q_gl, q_fluid_wells)
     prob = model.define_optimisation_problem()
-    variables = model.definir_variables()
-    modelo = model.construir_modelo()
-    restricciones = model.agregar_restricciones()
+    variables = model.define_variables()
+    #modelo = model.build_model()
+    #restricciones = model.agregar_restricciones()
+
+
     #resolve = model.resolver()
 
 
-    print(modelo)
-    #print(len(q_fluid_wells))
+    print(variables)
