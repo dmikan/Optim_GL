37d1f069ffa5b6a143f156edeadbdda06c2a63cd
andres lemus <leandres1985@gmail.com>
2025-03-15 17:52:36 +0100

    interpolator draft. Working but still to adapt in classes

--- interpolator.py
+++ interpolator.py
@@ -7,37 +7,70 @@ import matplotlib .pyplot as plt
 
 
 
+class Interpolator():
+    """Class implementing multiple interpolators"""
+
+    def __init__(self, x, y, method):
+        self.x = x
+        self.y = y
+        self.method = method
+
+
+    def linear_interpolator(self):
 
 
 
 
 
 if __name__ == "__main__":
+
+    # # linear interpolator
+    # path_data = './data/datos.csv'
+    # data = DataLoader(path_data)
+    # q_gl,q_fluid_wells = data.load_data()
+    # df = pd.DataFrame(q_fluid_wells)
+    # df = df.transpose()
+    # well1 = df[1]
+    # max_value_axis = max(q_gl)
+
+    # injection_rates_range = np.arange(0,max_value_axis,10)
+    # print(injection_rates_range)
+    # linterp = np.interp(injection_rates_range, q_gl, well1)
+
+    # #plt.plot(q_gl, well1, 'o', color= 'red')
+    # plt.plot(injection_rates_range, linterp)
+    # plt.show()
+
+
+
     path_data = './data/datos.csv'
     data = DataLoader(path_data)
     q_gl,q_fluid_wells = data.load_data()
-    # print(q_gl)
-    # print(q_fluid_wells)
     df = pd.DataFrame(q_fluid_wells)
     df = df.transpose()
-    # print(df)
-    well1 = df[1]
+    well1 = df[3]
     max_value_axis = max(q_gl)
 
-    #well1_interp = well1.interpolate(method='polynomial', order=2, inplace=True, limit_direction='both')
-    #spl = CubicSpline(q_gl, well1)
-    #spl = PchipInterpolator(q_gl, well1)
-    #spl = UnivariateSpline(q_gl, well1, s=10)
-
-
     injection_rates_range = np.arange(0,max_value_axis,10)
-    print(injection_rates_range)
+
+    spl = CubicSpline(q_gl, well1)
+    pchip_interp = PchipInterpolator(q_gl, well1)
+    univar_spl = UnivariateSpline(q_gl, well1, s=10)
+
     linterp = np.interp(injection_rates_range, q_gl, well1)
-    #interpolated_prod_rates = np.round(spl(injection_rates_range),0)
 
-    #print(linterp)
+    print(injection_rates_range)
 
+    interpolated_prod_rates_spl = np.round(spl(injection_rates_range),0)
+    interpolated_prod_rates_pchip = np.round(pchip_interp(injection_rates_range),0)
+    interpolated_prod_rates_univar = np.round(univar_spl(injection_rates_range),0)
+    interpolated_prod_rates_linterp = np.round(univar_spl(injection_rates_range),0)
 
     #plt.plot(q_gl, well1, 'o', color= 'red')
-    plt.plot(injection_rates_range, linterp)
+    #plt.plot(injection_rates_range, interpolated_prod_rates_spl, label='spl')
+    plt.plot(injection_rates_range, interpolated_prod_rates_pchip, label ='pchip')
+    #plt.plot(injection_rates_range, interpolated_prod_rates_univar, label='univar')
+    plt.plot(injection_rates_range, linterp, label='interp')
+
+    plt.legend()
     plt.show()
