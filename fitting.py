1d3830bd9724c7585398f1c94b3fc9aae7f3f619
Danny Perilla Mikán <dmikan@id.uff.br>
2025-04-04 09:53:14 -0500

    Added nishikiori data and new features

--- fitting.py
+++ fitting.py
@@ -13,19 +13,19 @@ class Fitting():
         self.q_oil = q_oil
 
     def fit(self, model):
-        params_list, _ = curve_fit(model, self.q_gl, self.q_oil, p0=[0, 0.1, 1, -10, 100])
+        params_list, _ = curve_fit(model, self.q_gl, self.q_oil)
         return params_list
 
-    def model_namdar(self, q_gl, a, b, c, d, e):
+    def model_namdar(self, q_gl_range, a, b, c, d, e):
         return (
-            a + b * q_gl + c * (q_gl ** 0.7) +
-            d * np.log(q_gl + 0.9) + e * np.exp(-(q_gl ** 0.6))
+            a + b * q_gl_range + c * (q_gl_range ** 0.7) +
+            d * np.log(q_gl_range + 0.9) + e * np.exp(-(q_gl_range ** 0.6))
         )
     
-    def model_dan(self, q_gl, a, b, c, d, e):
+    def model_dan(self, q_gl_range, a, b, c, d, e):
         return (
-            a + b * q_gl + c * (q_gl ** 0.7) +
-            d * np.log(q_gl + 1) + e * np.exp(-(q_gl ** 0.6))
+            a + b * q_gl_range + c * (q_gl_range ** 0.7) +
+            d * np.log(q_gl_range + 1) + e * np.exp(-(q_gl_range ** 0.6))
     )
 
     def plot_fitting(self, q_gl_range, y_pred, well):
@@ -40,15 +40,20 @@ class Fitting():
 
 if __name__ == "__main__":
     # Cargar datos
-    load = DataLoader('./data/gl_kanu_data.csv')
+    load = DataLoader('./data/gl_nishikiori_data_five.csv')
     q_gl_list, q_oil_list = load.load_data_gl_template()
 
     # Configurar la figura con 2 filas y 3 columnas, aumentando la altura
     fig, axes = plt.subplots(2, 3, figsize=(15, 12), constrained_layout=True)
     axes = axes.flatten()  # Para iterar fácilmente
 
-    # Generar valores escalados para graficar
-    q_gl_range = np.linspace(0, 4, 500)
+    # Asegurar que todos los valores en q_gl_list sean numéricos
+    q_gl_list = [[x for x in q_gl if not np.isnan(x)] for q_gl in q_gl_list]
+    q_oil_list = [[x for x in q_oil if not np.isnan(x)] for q_oil in q_oil_list]
+
+    # Ahora se puede calcular el máximo sin errores
+    q_gl_max = max([np.max(j) for j in q_gl_list])
+    q_gl_range = np.linspace(0, q_gl_max, 1000)
 
     # Crear un DataFrame con la primera columna como q_gl_range
     df_results = pd.DataFrame({"q_gl": q_gl_range})
@@ -76,7 +81,7 @@ if __name__ == "__main__":
         ax.scatter(q_gl, q_oil, color='red', label='Data points')
         ax.set_xlabel('q_gl')
         ax.set_ylabel('q_oil')
-        ax.set_title(f'Well {well}')
+        ax.set_title(f'Well {well+1}')
         ax.legend()
         ax.grid()
     plt.show()
